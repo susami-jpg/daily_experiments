107a597594fd988e9e543695d2614d09e4bf5c0b
kazuki susami <kazuki_susami@icloud.com>
2021-08-25 10:07:58 +0900

    admit nameless_name

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -6,6 +7,10 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(",")
+  end
+
+  def set_nameless_name
+    self.name = "名前なし" if name.blank?
   end
 
 end
