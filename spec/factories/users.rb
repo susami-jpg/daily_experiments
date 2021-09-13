74e59874804f2e9ae56e66ed88f9207dde2066d4
kazuki susami <kazuki_susami@icloud.com>
2021-09-14 01:57:09 +0900

    create request/users_spec.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { 'テストユーザー' }
     email { 'test1@example.com' }
+    admin { false }
     password { 'password' }
   end
 end
--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -2,28 +2,38 @@ require 'rails_helper'
 describe "ユーザー関連機能のテスト", type: :request do
   describe "admin_userでなければユーザー管理機能は使えない" do
     before do
-      @not_admin_user = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      @not_admin_user = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', admin: false)
       @other_user = FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-    end
 
-    it 'ユーザー一覧を見れない' do
-
-    end
-
-    it 'ユーザー詳細を見れない' do
-    
+      get login_path
+      expect(response).to have_http_status(:success)
+      post login_path, params: { session: { email: @not_admin_user.email, password: @not_admin_user.password } }
+      expect(response).to redirect_to root_path
     end
 
     it 'ユーザーを新たに作成できない' do
-
+      get new_admin_user_path
+      expect(response).to redirect_to root_path
+      expect do
+        post admin_users_path, params: { user: { name: 'ユーザーC', email: 'c@example.com', admin: true, password: 'password', password_confirmation: 'password'} }
+      end.not_to change(User, :count)
+      expect(response).to redirect_to root_path
     end
 
     it 'ユーザーを編集できない' do
-
+      get edit_admin_user_path(@other_user)
+      expect(response).to redirect_to root_path
+      patch admin_user_path(@other_user), params: { user: { name: 'ユーザーC', email: 'c@example.com', admin: true} }
+      expect(response).to redirect_to root_path
+      expect(@other_user.reload.name).to eq @other_user.name
+      expect(@other_user.reload.email).to eq @other_user.email
     end
 
     it 'ユーザーを削除できない' do
-
+      expect do
+        delete admin_user_path(@other_user)
+      end.not_to change(User, :count)
+      expect(response).to redirect_to root_path
     end
   end
 end
